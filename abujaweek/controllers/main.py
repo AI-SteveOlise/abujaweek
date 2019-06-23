0a77cfd664494300bd3347be34d1f929fe328be7
steve <solise@mceesolutions.com>
2019-06-23 21:51:23 +0100

    abuja week pyment gateway

--- abujaweek/controllers/main.py
+++ abujaweek/controllers/main.py
@@ -38,8 +38,15 @@ class Congratulations(http.Controller):
     
     @http.route('/event/payment_successful', type='http', auth='public', methods=['POST'], website=True, csrf=False)
     def show_congratulation_webpage(self, **kw):
+        order = request.website.sale_get_order(force_create=1)
         attendee_ids = set()
-        attendee_ids |= set(cart_values.get('attendee_ids', []))
+
+        registrations = self._process_registration_details(post)
+        for registration in registrations:
+            ticket = request.env['event.event.ticket'].sudo().browse(int(registration['ticket_id']))
+            cart_values = order.with_context(event_ticket_id=ticket.id, fixed_price=True)._cart_update(product_id=ticket.product_id.id, add_qty=1, registration_data=[registration])
+            attendee_ids |= set(cart_values.get('attendee_ids', []))
+            
         attendees = request.env['event.registration'].browse(list(attendee_ids))
         print(attendees)
         for attendee in attendees:
